b342701f38317323525a91de5ea8e25478263ffe
Peter Holak <peter.holak@pipelinersales.com>
2017-01-17 15:55:14 +0100

    fix checksum (chrome ungzipped my file)

--- Formula/patchelf.rb
+++ Formula/patchelf.rb
@@ -2,7 +2,7 @@ class Patchelf < Formula
     desc "PatchELF is a small utility to modify the dynamic linker and RPATH of ELF executables."
     homepage "http://nixos.org/patchelf.html"
     url "https://nixos.org/releases/patchelf/patchelf-0.9/patchelf-0.9.tar.gz"
-    sha256 "b0a96997382ec3597aee1376a9f6dfed04053c83f2b3d1062cc7b7cec4c6c95b"
+    sha256 "f2aa40a6148cb3b0ca807a1bf836b081793e55ec9e5540a5356d800132be7e0a"
 
     def install
         system "clang++", 
